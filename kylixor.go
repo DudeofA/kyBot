29a3626be603d7bcfbb0115a365ae8d3ee632f6e
AndrewLanghill <andrew.langhill@gmail.com>
2017-06-10 23:48:03 -0400

    Initial commit of files

---
+++ kylixor.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	//	"encoding/binary"
+	"flag"
+	"fmt"
+	//	"io"
+	// "log"
+	"os"
+	"os/signal"
+	//	"strings"
+	"syscall"
+	"time"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func init() {
+	flag.StringVar(&token, "t", "", "Bot Token")
+	flag.Parse()
+}
+
+var token string
+var logID = "296765401944162305"
+
+func main() {
+
+	if token == "" {
+		fmt.Println("No token provided. Please run: kylixor -t <bot token>")
+		return
+	}
+
+	// Create a new Discord session using the provided bot token.
+	ky, err := discordgo.New("Bot " + token)
+	if err != nil {
+		fmt.Println("Error creating Discord session: ", err)
+		return
+	}
+
+	// Register ready as a callback for the ready events.
+	ky.AddHandlerOnce(ready)
+
+	// Register messageCreate as a callback for the messageCreate events.
+	ky.AddHandler(messageCreate)
+
+	// Register other things
+	ky.AddHandler(messageEdit)
+	ky.AddHandler(messageDelete)
+	// ky.AddHandler(messageReactionAdd)
+	// ky.AddHandler(messageReactionRemove)
+
+	// Open the websocket and begin listening.
+	err = ky.Open()
+	if err != nil {
+		fmt.Println("Error opening Discord session: ", err)
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Kylixor is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	ky.Close()
+}
+
+// This function will be called (due to AddHandler above) when the bot receives
+// the "ready" event from Discord.
+func ready(s *discordgo.Session, event *discordgo.Ready) {
+
+	// Set the playing status.
+	s.UpdateStatus(0, "Alpha beta early access")
+}
+
+// This function will be called (due to AddHandler above) every time a new
+// message is created on any channel that the autenticated bot has access to.
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	if m.ChannelID != logID {
+		timestamp := time.Now()
+		logMessage(s, timestamp, m.Message.Author, m.ID, m.ChannelID, "MSG", m.ContentWithMentionsReplaced())
+	}
+
+	if m.Content == "ping" {
+		s.ChannelMessageSend(m.ChannelID, "Pong!")
+	}
+}
+
+func messageEdit(s *discordgo.Session, m *discordgo.MessageUpdate) {
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	if m.ChannelID != logID {
+		timestamp := time.Now()
+		logMessage(s, timestamp, m.Message.Author, m.ID, m.ChannelID, "EDI", m.ContentWithMentionsReplaced())
+	}
+}
+
+func messageDelete(s *discordgo.Session, m *discordgo.MessageDelete) {
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	if m.ChannelID != logID {
+		timestamp := time.Now()
+		logMessage(s, timestamp, m.Message.Author, m.ID, m.ChannelID, "DEL", m.ContentWithMentionsReplaced())
+	}
+}
---
+++ log.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func logMessage(s *discordgo.Session, timestamp time.Time, user *discordgo.User, mID, cID, code, message string) {
+
+	timestampf := timestamp.Format("Mon Jan 2 - 3:04PM")
+
+	var namestr string
+	channel, _ := s.State.Channel(cID)
+	member, err := s.State.Member(channel.GuildID, user.ID)
+	if err != nil {
+		namestr = user.Username + "#" + user.Discriminator
+	} else {
+		if member.Nick != "" {
+			namestr = member.Nick + " " + "(" + user.Username + "#" + user.Discriminator + ")"
+		} else {
+			namestr = user.Username + "#" + user.Discriminator
+		}
+	}
+
+	switch code {
+	case "MSG":
+		s.ChannelMessageSend(logID, fmt.Sprintf("```diff\n- %s - %s - %s:\n!MSG: %s\n```", timestampf, namestr, code, message))
+		break
+
+	case "EDI":
+		oldMsg, _ := s.State.Message(cID, mID)
+		s.ChannelMessageSend(logID, fmt.Sprintf("```diff\n- %s - %s - %s:\n!OLD: %s\n!NEW: %s\n```", timestampf, namestr, code, oldMsg.ContentWithMentionsReplaced(), message))
+		break
+
+	case "DEL":
+		oldMsg, _ := s.State.Message(cID, mID)
+		s.ChannelMessageSend(logID, fmt.Sprintf("```diff\n- %s - %s - %s:\n!MSG: %s\n```", timestampf, namestr, code, oldMsg))
+		break
+	default:
+		break
+	}
+}
