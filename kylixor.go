57c0160051211b580bdc2d4808d4bd463902a483
Andrew Langhill <andrew.langhill@gmail.com>
2019-05-05 13:05:37 -0400

    Test fix

--- kylixor.go
+++ kylixor.go
@@ -399,7 +399,7 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 					status,
 					"https://www.factorio.com/static/img/factorio-logo.png",
 					"https://www.factorio.com/static/img/factorio-wheel.png",
-					time.Now().Format(time.RFC3339),
+					time.Now(),
 				)
 
 				//Edit previous server status message
@@ -470,7 +470,7 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 					status,
 					"https://www.freepnglogos.com/uploads/minecraft-logo-6.png",
 					"https://logodix.com/logo/1014674.png",
-					time.Now().Format(time.RFC3339),
+					time.Now(),
 				)
 
 				//Edit previous server status message
@@ -590,7 +590,7 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 func (embed *discordgo.MessageEmbed) CreateEmbed(title string, color int, desc string, f1_name string, f1_value, f2_name string, f2_value, image string, thumbnail string, timestamp time) {
 
 	//Create and update embeded status message
-	embed := &discordgo.MessageEmbed{
+	embed = &discordgo.MessageEmbed{
 		Author:      &discordgo.MessageEmbedAuthor{},
 		Color:       color, //factorio color
 		Description: desc,
@@ -612,7 +612,7 @@ func (embed *discordgo.MessageEmbed) CreateEmbed(title string, color int, desc s
 		Thumbnail: &discordgo.MessageEmbedThumbnail{
 			URL: thumbnail,
 		},
-		Timestamp: time, // Discord wants ISO8601; RFC3339 is an extension of ISO8601 and should be completely compatible.
+		Timestamp: time.Format(time.RFC3339), // Discord wants ISO8601; RFC3339 is an extension of ISO8601 and should be completely compatible.
 		Title:     title,
 	}
 }
