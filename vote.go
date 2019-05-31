cf64b6c6f0dbd7d448f8a0721f0283265fc5c144
Andrew Langhill <andrew.langhill@gmail.com>
2019-05-31 00:26:18 -0400

    More work on vote commands

--- vote.go
+++ vote.go
@@ -9,6 +9,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
@@ -16,16 +17,23 @@ import (
 
 //startVote - begin a vote with variable vote options
 func startVote(s *discordgo.Session, m *discordgo.MessageCreate, data string) bool {
-	array := strings.SplitAfter(data, " ")
+	//Parse the incoming command into data and strings
+	array := strings.SplitAfterN(data, " ", 1)
 	options := array[0]
 	text := array[1]
+	optionNum, err := strconv.Atoi(options)
+	if err != nil {
+		panic(err)
+	}
 
-	switch options {
-	case "1":
+	//How many vote options
+	switch optionNum {
+	case 1:
 		s.ChannelMessageSend(m.ChannelID, "Starting vote...Upvote/Downvote to cast your vote")
 		quoteMsg, _ := s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("```\n%s\n```", text))
 		s.MessageReactionAdd(m.ChannelID, quoteMsg.ID, "upvote:402490285365657600")
 		s.MessageReactionAdd(m.ChannelID, quoteMsg.ID, "downvote:402490335789318144")
+		//GetVoteResults(s, m, optionNum)
 		break
 
 	default:
